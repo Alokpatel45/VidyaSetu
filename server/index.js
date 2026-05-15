d01393620dfd736a2fe5ae6c979ed94693676477
ALOK PATEL <alokpatelmnnit@gmail.com>
2026-05-15 14:37:49 +0530

    origin fix

--- server/index.js
+++ server/index.js
@@ -18,7 +18,8 @@ dotenv.config();
 
 const defaultOrigins = [
   "https://www.vidyasetu.me",
-  "https://vidya-setu-frontend-ui-ruddy.vercel.app",
+  "https://vidya-setu-frontend-ui-git-main-alok-patels-projects-a7d43281.vercel.app",
+  "https://vidya-setu-frontend-e1qqbaosg-alok-patels-projects-a7d43281.vercel.app",
 ];
 
 const allowedOrigins = [
